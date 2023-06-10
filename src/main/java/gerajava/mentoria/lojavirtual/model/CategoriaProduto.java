885854afb98a21f49b01ef7feb1f1c7278e237ad
gerajava <gerampereira67@gmail.com>
2023-06-10 15:27:45 -0300

    implementação classes modelo mentoria

--- src/main/java/gerajava/mentoria/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/gerajava/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -1,9 +1,66 @@
 package gerajava.mentoria.lojavirtual.model;
 
-public class CategoriaProduto {
+import java.io.Serializable;
+import java.util.Objects;
 
-	public CategoriaProduto() {
-		// TODO Auto-generated constructor stub
-	}
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+@Entity
+@Table(name="categoria_produto")
 
+@SequenceGenerator(name="seq_categotia_produto",sequenceName="seq_categotia_produto",allocationSize = 1,initialValue = 1)
+public class CategoriaProduto implements Serializable {
+	
+private static final long serialVersionUID=1L;
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "seq_categotia_produto")
+private long Id;
+ 
+ @Column(name="nome_desc_cat",nullable = false)
+ private String nomeDesc;
+
+public long getId() {
+	return Id;
+}
+
+public void setId(long id) {
+	Id = id;
+}
+
+public String getNomeDesc() {
+	return nomeDesc;
+}
+
+public void setNomeDesc(String nomeDesc) {
+	this.nomeDesc = nomeDesc;
+	
+	
+	
+}
+
+@Override
+public int hashCode() {
+	return Objects.hash(Id);
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	CategoriaProduto other = (CategoriaProduto) obj;
+	return Id == other.Id;
+}
+ 
+ 
+ 
+ 
 }
---
+++ src/main/java/gerajava/mentoria/lojavirtual/model/PessoaJuridica.java
@@ -0,0 +1,58 @@
+package gerajava.mentoria.lojavirtual.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+@PrimaryKeyJoinColumn(name="id")
+public class PessoaJuridica extends Pessoa{
+
+	private static final long serialVersionUID = 1L;
+
+	private String cnpj;
+	private String inscEstadual;
+	private String inscMunicipal;
+	private String nomeFantasia;
+	private String razaoSocial;
+	private String categotia;
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getInscEstadual() {
+		return inscEstadual;
+	}
+	public void setInscEstadual(String inscEstadual) {
+		this.inscEstadual = inscEstadual;
+	}
+	public String getInscMunicipal() {
+		return inscMunicipal;
+	}
+	public void setInscMunicipal(String inscMunicipal) {
+		this.inscMunicipal = inscMunicipal;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getCategotia() {
+		return categotia;
+	}
+	public void setCategotia(String categotia) {
+		this.categotia = categotia;
+	}
+	
+	
+}
